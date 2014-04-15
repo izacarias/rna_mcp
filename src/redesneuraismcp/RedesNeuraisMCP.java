72374901d5892500442156ce177b51a8be674b15
Iulisloi Zacarias <iuli.zacarias@gmail.com>
2014-04-15 01:22:07 -0300

    Segunda Versão com XOR e 5000 ciclos de treinamento.

--- src/redesneuraismcp/RedesNeuraisMCP.java
+++ src/redesneuraismcp/RedesNeuraisMCP.java
@@ -17,49 +17,59 @@ public class RedesNeuraisMCP {
     public static void main(String[] args) {
         Neuronio neuroAND = new Neuronio();
         Neuronio neuroOR = new Neuronio();
+        Neuronio neuroXOR = new Neuronio();
+
         // { IN1, IN2, OUT }
         int[][] andPort = {
-            {0, 0, 0}, 
-            {0, 1, 0}, 
-            {1, 0, 0}, 
+            {0, 0, 0},
+            {0, 1, 0},
+            {1, 0, 0},
             {1, 1, 1}
         };
-        
+
         int[][] orPort = {
-            {0, 0, 0}, 
-            {0, 1, 1}, 
-            {1, 0, 1}, 
+            {0, 0, 0},
+            {0, 1, 1},
+            {1, 0, 1},
             {1, 1, 1}
         };
-        
-        RedesNeuraisMCP.treinaRNA(andPort, neuroAND, "AND");
-        RedesNeuraisMCP.treinaRNA(andPort, neuroAND, "AND");
+
+        int[][] xorPort = {
+            {0, 0, 0},
+            {0, 1, 1},
+            {1, 0, 1},
+            {1, 1, 0}
+        };
+
+        for (int i = 0; i < 5000; i++) {
+            RedesNeuraisMCP.treinaRNA(andPort, neuroAND, "AND");
+            RedesNeuraisMCP.treinaRNA(orPort, neuroOR, "OR");
+            RedesNeuraisMCP.treinaRNA(xorPort, neuroXOR, "XOR");
+        }
+
         RedesNeuraisMCP.testaRNA(andPort, neuroAND, "AND");
-        
-        RedesNeuraisMCP.treinaRNA(orPort, neuroOR, "OR");
-        RedesNeuraisMCP.treinaRNA(orPort, neuroOR, "OR");
         RedesNeuraisMCP.testaRNA(orPort, neuroOR, "OR");
-        
-        
+        RedesNeuraisMCP.testaRNA(xorPort, neuroXOR, "XOR");
+
     }
-    
-    private static void testaRNA(int[][] tabelaVerdade, Neuronio n, 
-            String porta){
-        
-        System.out.println(" -------- Teste RNA ("+ porta +") -----------");
+
+    private static void testaRNA(int[][] tabelaVerdade, Neuronio n,
+            String porta) {
+
+        System.out.println(" -------- Teste RNA (" + porta + ") -----------");
         for (int[] linha : tabelaVerdade) {
             System.out.printf("|%1d", linha[0]);
-            System.out.print(" "+porta+" ");
+            System.out.print(" " + porta + " ");
             System.out.printf("%1d", linha[1]);
             System.out.printf(" = %1d|", n.run(linha[0], linha[1]));
             System.out.println();
         }
     }
-    
-    private static void treinaRNA(int[][] tabelaVerdade, Neuronio n, 
-            String porta){
-        
-        System.out.println(" Treinando a RNA ("+ porta +")...");
+
+    private static void treinaRNA(int[][] tabelaVerdade, Neuronio n,
+            String porta) {
+
+        System.out.println(" Treinando a RNA (" + porta + ")...");
         for (int[] linha : tabelaVerdade) {
             int[] entrada = new int[2];
             entrada[0] = linha[0];
@@ -67,6 +77,6 @@ public class RedesNeuraisMCP {
             int result = linha[2];
             n.aprendizagem(entrada, result);
         }
-    }   
+    }
 
 }
